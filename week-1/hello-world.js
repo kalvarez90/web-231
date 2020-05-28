a3ae72900304bd35a152e44b0dbd38b5d5fa742b
Karina Alvarez <alvarez.webdeveloper@gmail.com>
2020-05-27 23:04:51 -0400

    Adding commit

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,1 +1,9 @@
-console.log("Hello World, my name is Karina Alvarez")
+/*
+*Title: Assignment 1.1
+*Author: Professor Krasso
+*Date: 27 May 2020
+*Modified By: Karina Alvarez
+*Description: Hello World Assignment. Week 1
+*/
+//Following professor Krasso instructions
+console.log("Hello World, my name is Karina Alvarez");
