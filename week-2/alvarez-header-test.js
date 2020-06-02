9ddc6286ff985067fc3e2e420e46eb0b47309355
Karina Alvarez <alvarez.webdeveloper@gmail.com>
2020-06-02 17:57:47 -0400

    inital commit

---
+++ week-2/alvarez-header-test.js
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title:  Exercise 2.2 Header
+; Author: Professor Krasso
+; Date:   2 June 2020
+; Modified by: Karina Alvarez
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('./alvarez-header.js');
+
+console.log(header.display("Karina", "Alvarez", "Exercise 2.2 - Header"));
