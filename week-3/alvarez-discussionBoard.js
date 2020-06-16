53a4046588181b744cf42c462395c75436cca624
Karina Alvarez <alvarez.webdeveloper@gmail.com>
2020-06-16 18:50:44 -0400

    Commit

---
+++ week-3/alvarez-discussionBoard.js
@@ -0,0 +1,24 @@
+const header = require('./alvarez-header.js');
+
+console.log(header.display("Karina", "Alvarez", "Exercise 3.1 - Control Statements"));
+console.log ('\n');
+/*=======================================
+; Title: Discussion Board 3.1
+; Author: Professor Krasso
+; Date: 9 June 2020
+; Modify by: Karina Alvarez
+; Description: Javascript program using
+; for statement. Three errors present.
+=======================================*/
+
+// Using 'for' statement
+// two errors in the following statement
+let number = '';
+
+for (let i = 0; i < 11; i+)
+  number += i + "\n";
+}
+
+// Output is numbers 0 - 10. Each in a new line.
+// One error present in the following statement.
+console.log(Number);
---
+++ week-4/alvarez-discussionBoard.js
@@ -0,0 +1,31 @@
+const header = require('./alvarez-header.js');
+
+console.log(header.display("Karina", "Alvarez", "Exercise 4.1 - Arrays"));
+console.log ('\n');
+/*=======================================
+; Title: Discussion Board 4.1
+; Author: Professor Krasso
+; Date: 16 June 2020
+; Modify by: Karina Alvarez
+; Description: Javascript program using
+; arrays. Three errors present.
+=======================================*/
+
+// different variables specified at once
+var favoriteShows, text, SLen, i;
+
+// array of 4 different movies/Tv shows
+//one error
+favoriteShows = ["Mr. Robot", "Dark", "Money Heist", "The Good Doctor";
+FavLen = favoriteShows.length;
+
+//the for loop will go through each array.
+// one error
+text = "";
+for (i = 0; i < FavLen; i++)
+  text += favoriteShows[i] + '\n';
+}
+
+// Output should be the 4 shows specified in favoriteShows in a different line each.
+// one error
+console.log(text());
