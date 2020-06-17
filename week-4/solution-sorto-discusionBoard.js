b79c94f0a783c8eaa4fa82bec40f356266941c22
Karina Alvarez <alvarez.webdeveloper@gmail.com>
2020-06-17 17:05:01 -0400

    updated

--- week-4/solution-sorto-discusionBoard.js
+++ week-4/solution-sorto-discusionBoard.js
@@ -1,31 +1,31 @@
 
-  	============================================
-  	 
+  	/*============================================
+
   	; Title: Discussion Board 4.1 - Arrays
   	; Author: Arlix Sorto
   	; Date: 6/16/2020
-  	; Modified By:
+  	; Modified By: Karina Alvarez
   	; Description: A program with a few of errors.
-  	 
+
   	;===========================================
   	*/
-  	 
-  	 
-  	//FIXME: Should output a list of colors. 
-  	 
+
+
+  	//FIXME: Should output a list of colors.
+
      // changed {} for []
   	var myColors = ["Black","Blue", "Brown", "Gray", "Green","Orange", "Purple", "Red","White", "Yellow"];
-  	 
+
   	//Validate an array and then loop through an array.
   	function displayColors(colors){
-  	 
+
   	if(Array.isArray(colors)){
   	for(var i = 0; i< colors.length; i++){  // errased () after colors.length
   	console.log(colors[i]); // changed {} for []
   	}
   	}
   	}
-  	 
+
   	//Calls displayColors function
   	displayColors(myColors);
-  	 
+
