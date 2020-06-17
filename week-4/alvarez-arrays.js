ac3c9b50ebc46107c04641251816f5d10f03e14f
Karina Alvarez <alvarez.webdeveloper@gmail.com>
2020-06-17 14:52:12 -0400

    Initial commit

---
+++ week-4/alvarez-arrays.js
@@ -0,0 +1,28 @@
+const header = require('./alvarez-header.js');
+
+console.log(header.display("Karina", "Alvarez", "Exercise 4.2 - Arrays"));
+console.log ('\n');
+/*
+============================================
+; Title:  Arrays
+; Author: Professor Krasso
+; Date:   17 June 2020
+; Modified By: Karina Alvarez
+; Description: Demonstrates how to define and output array values
+;===========================================
+*/
+
+// start of code
+
+// array of fruit values
+var fruits = ["Mango", "Apple", "Watermelon", "Banana", "Orange"];
+
+//function with for loop
+function getFruit(arr) {
+  for (let x = 0; x < arr.length; x++) {
+    console.log(arr[x]);
+  }
+}
+
+// call the getFruit() function and output the contents of the array
+getFruit(fruits);
