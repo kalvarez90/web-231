9af292f9f02a5f179e0969382810dffbbefd3ed4
Karina Alvarez <alvarez.webdeveloper@gmail.com>
2020-06-23 17:33:46 -0400

    commit

---
+++ week-5/solution-brooklyn-discusionBoard.js
@@ -0,0 +1,23 @@
+ /*
+  	============================================
+  	; Title: hairston-discussion.js
+  	; Author: Brooklyn Hairston
+  	; Date: 23 June 2020
+  	; Modified by: Brooklyn Hairston
+  	; Description: Program using key collection array
+  	; with two errors
+  	;===========================================
+  	*/
+
+
+
+
+ let stopLight = new Map()
+
+ stopLight.set("green", "go");
+ stopLight.set("red", "stop");
+ stopLight.set("yellow", "caution");
+
+ for (let [key, value] of stopLight) {
+  console.log("a " + key + " light" + " means " + value);
+ }
