006effd9f010c9a681934df105dccef2e3865309
Karina Alvarez <alvarez.webdeveloper@gmail.com>
2020-06-23 17:38:33 -0400

    commit

--- week-5/solution-brooklyn-discusionBoard.js
+++ week-5/solution-brooklyn-discusionBoard.js
@@ -1,28 +1,30 @@
  /*
-  	============================================
-  	; Title: hairston-discussion.js
-  	; Author: Brooklyn Hairston
-  	; Date: 23 June 2020
-  	; Modified by: Brooklyn Hairston
-  	; Description: Program using key collection array
-  	; with two errors
-  	;===========================================
-  	*/
+      ============================================
+      ; Title: hairston-discussion.js
+      ; Author: Brooklyn Hairston
+      ; Date: 23 June 2020
+      ; Modified by: Brooklyn Hairston
+      ; Description: Program using key collection array
+      ; with two errors
+      ;===========================================
+      */
 
-  	//header
-  	const header = require('./hairston-header.js')
+      //header
+      const header = require('./hairston-header.js')
 
     console.log(header.display('Brooklyn','Hairston', 'Discussion'))
 
     //program with two errors
 
 
- let stopLight = new Map()
+ let stopLight = new Map() // separated new from Map()
 
+// added semicolon at the end
  stopLight.set("green", "go");
  stopLight.set("red", "stop");
  stopLight.set("yellow", "caution");
 
+// added brackets [ ] to key and value
  for (let [key, value] of stopLight) {
   console.log("a " + key + " light" + " means " + value);
  }
