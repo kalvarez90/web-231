ef82ba678047228de1ffa682a331e2a60ecfd3d8
Karina Alvarez <alvarez.webdeveloper@gmail.com>
2020-06-24 15:14:02 -0400

    commit

---
+++ week-5/solution-douglas-discussionBoard.js
@@ -0,0 +1,19 @@
+
+/* ============================================
+; Title: Assignment 5.1
+; Author: Douglas Jenkins
+; Date: 23 June 2020
+; Modified By: Karina Alvarez
+; Description: fix the mistakes
+;=========================================== */
+
+// Three mistakes below
+// Creates a new map and set what is being passed
+let fruitColor = new Map ();
+fruitColor.set ( 'Apple' , 'Red' );
+fruitColor,set ( 'Pear' , 'Green' );
+
+// Displays the value of the new Map
+for ( lets [key , value] of fruitColor) {
+console.logs ('The ' + key + ' is ' + value );
+}
